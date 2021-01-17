a62a9c3725dfa4ef753287c57be2765a12303c28
Pep Jebs <nrohn@tx.rr.com>
2021-01-17 13:11:27 -0800

    Fabric mod shell

---
+++ src/main/java/pepjebs/choruslinks/ChorusLinksMod.java
@@ -0,0 +1,10 @@
+package pepjebs.choruslinks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ChorusLinksMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
